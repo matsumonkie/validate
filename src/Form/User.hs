4fce105776a7ade1837313a63c1f2dd9ad4463c7
Iori Matsuhara <matsuhar@gmail.com>
2015-10-20 11:40:00 +0200

    list without O(n) creation

--- src/Form/User.hs
+++ src/Form/User.hs
@@ -10,18 +10,18 @@ userForm :: User -> Form User
 userForm user =
   let
     form = mkForm user deals
-    deals =
-      mkDeal "firstname" presence (firstname user) :
-      mkDeal "lastname" presence (lastname user) :
-      mkDeal "age" (Coerce "positiveAge" (>= 0)) (age user) :
-      []
+    deals = [
+      mkDeal "firstname" presence (firstname user),
+      mkDeal "lastname" presence (lastname user),
+      mkDeal "age" (Coerce "positiveAge" (>= 0)) (age user)
+      ]
   in form
 
 adultForm :: User -> Form User
 adultForm user =
   let
     form = mkForm user deals
-    deals =
-      mkDeal "age" (Coerce "adultAge" (>= 18)) (age user) :
-      []
+    deals = [
+      mkDeal "age" (Coerce "adultAge" (>= 18)) (age user)
+      ]
   in bindForm (userForm user) form
